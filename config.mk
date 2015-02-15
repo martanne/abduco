fd26003cce5a68498b8f70bf38944da983630852
Marc André Tanner <mat@brain-dump.org>
2015-02-15 10:00:21 +0100

    Cleanup config.mk, introduce $CPPFLAGS
    $CPPFLAGS does only contain feature test macro defines, thus one
    system which need different ones compilation should succeed with
    something like:
    
     make CPPFLAGS= LIBS=-lc

--- config.mk
+++ config.mk
@@ -9,17 +9,11 @@ MANPREFIX = ${PREFIX}/share/man
 INCS = -I.
 LIBS = -lc -lutil
 
-# AIX
-#LIBS = -lc
-
-# Solaris
-#LIBS = -lc -lsocket
-
-CFLAGS += -std=c99 -Os ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=700
-
+CPPFLAGS = -D_POSIX_C_SOURCE=200809L
+CFLAGS += -std=c99 -pedantic -Wall ${INCS} -DVERSION=\"${VERSION}\" -DNDEBUG ${CPPFLAGS}
 LDFLAGS += ${LIBS}
 
-DEBUG_CFLAGS = ${CFLAGS} -UNDEBUG -O0 -g -ggdb -Wall
+DEBUG_CFLAGS = ${CFLAGS} -UNDEBUG -O0 -g -ggdb
 
-CC = cc
-STRIP = strip
+CC ?= cc
+STRIP ?= strip
