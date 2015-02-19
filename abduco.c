ad05af340ac17b837bfeed7c2f31ae88f8b6ddb3
Marc André Tanner <mat@brain-dump.org>
2015-02-19 17:46:05 +0100

    Change some year numbers to 2015

--- abduco.c
+++ abduco.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013-2014 Marc André Tanner <mat at brain-dump.org>
+ * Copyright (c) 2013-2015 Marc André Tanner <mat at brain-dump.org>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -514,7 +514,7 @@ int main(int argc, char *argv[]) {
 			client.readonly = true;
 			break;
 		case 'v':
-			puts("abduco-"VERSION" © 2013-2014 Marc André Tanner");
+			puts("abduco-"VERSION" © 2013-2015 Marc André Tanner");
 			exit(EXIT_SUCCESS);
 		default:
 			usage();
