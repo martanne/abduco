c6b2e7da31ddde0a0052c8fd212c88a5236734b6
Stefano Frabetti <stefano.frabetti@gmail.com>
2016-01-31 19:08:52 +0100

    Clarify KEY_REDRAW default value (i.e. disabled)

--- config.def.h
+++ config.def.h
@@ -2,7 +2,8 @@
 #define ABDUCO_CMD "dvtm"
 /* default detach key, can be overriden at run time using -e option */
 static char KEY_DETACH = CTRL('\\');
-/* redraw key to send a SIGWINCH signal to underlying process (unused by default) */
+/* redraw key to send a SIGWINCH signal to underlying process
+ * (set to 0 to disable the redraw key) */
 static char KEY_REDRAW = 0;
 /* Where to place the "abduco" directory storing all session socket files.
  * The first directory to succeed is used. */
